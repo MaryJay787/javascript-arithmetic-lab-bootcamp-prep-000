610b4e753e7aa5f9e6e56520105eefbf2009160a
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-18 21:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function dec(n) {
   return n -= 1;
 }
 
-function makeInt(n, a) {
-  parseInt('0.2328', 10);
-  return 0;
+function makeInt(n) {
+  return parseInt('n', 10);
 }
 
 function preserveDecimal(n) {
